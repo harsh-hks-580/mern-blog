45dec4df4aeccd01fd166707b3360579e39962db
Harsh Singh <harshksingh08@gmail.com>
2024-07-12 14:14:16 +0530

    Initial Commmit

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+          <div className="image">
+            <img src="https://techcrunch.com/wp-content/uploads/2024/07/brianstack-mspy-email-leak-2024.jpg?resize=1200,675"/>
+          </div>
+          <div className="texts">
+            <h2>Visual AI models might not see anything at all</h2>
+            <p className="info">
+              <a className="author">Harsh Singh</a>
+              <time>2024-07-16 02:58</time>
+            </p>
+            <p className="summary">Today at its special launch event, home backup power</p>
+          </div>
+        </div>
+    )
+}
